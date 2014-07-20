9fcab03d70e4f0ebf3bc25e07f4332d54ca753cd
Michael Symonds <talkinnsmak@gmail.com>
2014-07-20 01:15:40 -0700

    minor tweak and test
    Fixed collider for enemy ship to capsule collider as mesh collider will
    not work (no collider asset to link to like the player model so no
    reference for collision. As a result, player can fly "through" the
    enemy)
    Also tried creating a "camera mover" script in an attempt to change the
    camera view upon boss spawn. No success yet. :-(

---
+++ Assets/Scripts/CameraMover.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMover : MonoBehaviour {
+
+	private GameController gameController;
+	public Vector3 offset;
+
+	void Start()
+	{
+
+		if (gameController.bossActive) 
+		{
+			transform.position += offset;
+		} //if
+	} //moveCamera
+} //CameraMover : MonoBehaviour
