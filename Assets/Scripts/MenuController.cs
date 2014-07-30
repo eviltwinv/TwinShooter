65a4e9f66158a0280dd772b560479a34a483c561
Michael Symonds <talkinnsmak@gmail.com>
2014-07-29 22:23:54 -0700

    Basic Main Menu Built
    And I mean super SUPER basic

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuController : MonoBehaviour {
+
+	public AudioSource[] audioSource;
+	private int shotAudioIndex = 0;
+
+	// Use this for initialization
+	void Start () {
+		audioSource[shotAudioIndex].Play();
+	}
+	
+
+}
---
+++ Assets/Scripts/TextController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TextController : MonoBehaviour {
+
+
+	public bool isQuitButton = false;
+
+	void OnMouseEnter()
+	{
+		//change color of text
+		renderer.material.color = Color.green;
+	}
+
+	void OnMouseExit()
+	{
+		renderer.material.color = Color.white;
+	}
+
+	void OnMouseUp()
+	{
+		//is this a quit button?
+		if (isQuitButton) 
+		{
+			//quit the game	
+			Application.Quit ();
+		} 
+		else 
+		{
+			//load the level
+			Application.LoadLevel(1);
+		}
+
+	}
+}
